b18d76a54da41be8560a656a9a01e0c97d7e8c74
Sharma-lakshay <72092451+Sharma-lakshay@users.noreply.github.com>
2021-04-11 22:50:09 +0530

    Add files via upload

---
+++ fruitbasket.js
@@ -0,0 +1,18 @@
+img= "";
+
+function preload(){
+    img= loadImage("fruitbasket.jpg");
+}
+
+function setup(){
+    canvas= createCanvas(640, 420);
+    canvas.position(300, 100);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function backtohome(){
+    window.location= "index.html";
+}
